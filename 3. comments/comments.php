b6a9f14c096aecca293ff4fd943bd879638afcdf
abiyyu03 <abiyyu652@gmail.com>
2022-04-12 17:39:58 +0700

    first commit

---
+++ 3. comments/comments.php
@@ -0,0 +1,18 @@
+<?php
+
+//one line comment using double slash
+# one line comment using hash sign
+$a = 20; 
+
+
+/**
+ * This is multi line comment 
+ */
+echo $a;
+
+/**
+ * 3 Tips to write meaningful comment
+ * 1) making the code speak for its self by naming the meaningful identifiers
+ * 2) Write comment to explain why it does do instead what code does
+ * 3) make it as concise as posible (ringkas)
+ */
---
+++ 4. constants/constant.php
@@ -0,0 +1,8 @@
+<?php
+//execute during run time
+define('PHI', 3.14);
+
+//or like this
+//execute during compile time
+const CMYK = ['Cyan','Maroon','Yellow','Black'];
+const PHI2 = 22.7;
